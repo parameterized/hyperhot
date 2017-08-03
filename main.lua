2bef6585c4fe381cee0adcc3d305128d0dfa9aa1
parameterized <troyk212@gmail.com>
2017-08-03 19:42:49 -0400

    +basic project files

---
+++ main.lua
@@ -0,0 +1,19 @@
+require 'camera'
+
+ssx = love.graphics.getWidth()
+ssy = love.graphics.getHeight()
+
+love.filesystem.setIdentity(love.window.getTitle())
+math.randomseed(love.timer.getTime())
+
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
